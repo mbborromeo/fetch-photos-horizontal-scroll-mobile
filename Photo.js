f277507e782ea40d466469b54e304ac9bdbc4f71
Michael Borromeo <mbborromeo@gmail.com>
2019-12-11 18:24:03 -0800

    refactor Photo into a component

---
+++ Photo.js
@@ -0,0 +1,89 @@
+import React, { useState , useEffect, useMemo, useCallback } from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+function Photo( props ) {
+    console.log("Photo!!!!");
+
+    // add Styles here...
+    const styles = StyleSheet.create({
+        imageContainer: {
+            width: props.windowWidth, // dynamically gets window width, so app will work on all iOS phones
+            height: 'auto',
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-around', // main-axis
+            alignItems: 'center', // cross-axis
+        },
+
+        imageBackground: { // positioned absolute by default, so ignores border properties
+            width: '100%', 
+            height: 'auto', 
+            resizeMode: 'contain',
+            aspectRatio: 1, 
+            flex: 1,
+            justifyContent: 'center',
+            shadowColor: 'black',
+            shadowOffset: { width:0, height:3 },
+            shadowOpacity: 1,
+            shadowRadius: 2,        
+        },
+
+        innerImage: { // needed to override border properties of imageBackground
+            borderRadius: 16, 
+            borderColor: 'black', 
+            borderWidth: 1,
+        },
+
+        titleWrapper: {
+            marginTop: 'auto',
+            marginBottom: 'auto',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+
+        title: {
+            fontSize: 24,
+            color: 'black',
+            backgroundColor: 'white',
+            opacity: 0.7,
+            padding: 16,
+            borderColor: 'grey',
+            borderWidth: 1,
+            transform: [{ rotate: '-45deg' }],
+            textAlign: 'left',
+        },
+    });
+
+    return (  
+      <View style={ styles.imageContainer }>                      
+          <ImageBackground  
+              source={{ 
+                  uri: props.item.url,
+                  cache: 'force-cache',
+              }}  
+              style={ styles.imageBackground } 
+              imageStyle={ styles.innerImage }
+              resizeMode={ 'contain' }                       
+              // loadingIndicatorSource={[ require('./assets/loading_icons8com_2.gif') ]}
+              // loadingIndicatorSource={{uri: require('./assets/loading_icons8com_2.gif')}}
+              // loadingIndicatorSource={ require('./assets/loading_icons8com_2.gif') }
+              /*
+              defaultSource={{ 
+                  uri: require('./assets/loading_icons8com_2.gif'),
+                  width: 60,
+                  height: 60,
+              }}
+              */
+              defaultSource={ require('./assets/loading_icons8com_16.gif') }
+          >
+              <View style={ styles.titleWrapper }>
+                  <Text style={ styles.title }>{ props.item.title }</Text>
+              </View>
+          </ImageBackground>
+      </View>
+    );  
+}
+
+
+
+export default Photo;
--- PhotosList.js
+++ PhotosList.js
@@ -1,6 +1,7 @@
 import React, { useState , useEffect, useMemo, useCallback } from 'react';
 import { StyleSheet, Text, View, ImageBackground, FlatList, Button, Alert, ActivityIndicator, Dimensions } from 'react-native';
 import PhotosService from './PhotosService';
+import Photo from './Photo';
 import * as Utils from './utils';
 
 function PhotosList( props ) {
@@ -49,53 +50,6 @@ function PhotosList( props ) {
             // justifyContent: 'center'
         },
 
-        imageContainer: {
-            width: width, // dynamically gets window width, so app will work on all iOS phones
-            height: 'auto',
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'space-around', // main-axis
-            alignItems: 'center', // cross-axis
-        },
-      
-        imageBackground: { // positioned absolute by default, so ignores border properties
-            width: '100%', 
-            height: 'auto', 
-            resizeMode: 'contain',
-            aspectRatio: 1, 
-            flex: 1,
-            justifyContent: 'center',
-            shadowColor: 'black',
-            shadowOffset: { width:0, height:3 },
-            shadowOpacity: 1,
-            shadowRadius: 2,        
-        },
-
-        innerImage: { // needed to override border properties of imageBackground
-            borderRadius: 16, 
-            borderColor: 'black', 
-            borderWidth: 1,
-        },
-
-        titleWrapper: {
-            marginTop: 'auto',
-            marginBottom: 'auto',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-
-        title: {
-            fontSize: 24,
-            color: 'black',
-            backgroundColor: 'white',
-            opacity: 0.7,
-            padding: 16,
-            borderColor: 'grey',
-            borderWidth: 1,
-            transform: [{ rotate: '-45deg' }],
-            textAlign: 'left',
-        },
-
         /*
         button: {
           backgroundColor: '#000033',
@@ -123,33 +77,8 @@ function PhotosList( props ) {
     const renderItemHandler = useCallback(
         ({item}) => {
             console.log("renderItemHandler");
-            return (  
-              <View style={ styles.imageContainer }>                      
-                  <ImageBackground  
-                      source={{ 
-                          uri: item.url,
-                          cache: 'force-cache',
-                      }}  
-                      style={ styles.imageBackground } 
-                      imageStyle={ styles.innerImage }
-                      resizeMode={ 'contain' }                       
-                      // loadingIndicatorSource={[ require('./assets/loading_icons8com_2.gif') ]}
-                      // loadingIndicatorSource={{uri: require('./assets/loading_icons8com_2.gif')}}
-                      // loadingIndicatorSource={ require('./assets/loading_icons8com_2.gif') }
-                      /*
-                      defaultSource={{ 
-                          uri: require('./assets/loading_icons8com_2.gif'),
-                          width: 60,
-                          height: 60,
-                      }}
-                      */
-                      defaultSource={ require('./assets/loading_icons8com_16.gif') }
-                  >
-                      <View style={ styles.titleWrapper }>
-                          <Text style={ styles.title }>{ item.title }</Text>
-                      </View>
-                  </ImageBackground>
-              </View>
+            return ( 
+              <Photo item={ item } windowWidth={ width } />
             );  
         },
         [styles.imageBackground, styles.imageContainer, styles.innerImage, styles.title, styles.titleWrapper]
