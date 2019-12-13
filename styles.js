6a9e27e4f95217c8ec9a9b87469e012c5088321a
Michael Borromeo <mbborromeo@gmail.com>
2019-12-12 19:36:14 -0800

    edit styles export default

--- styles.js
+++ styles.js
@@ -1,7 +1,8 @@
 import { StyleSheet } from 'react-native';
 
+// export default StyleSheet.create({ 
 // export default const styles = StyleSheet.create({ 
-export default StyleSheet.create({ 
+export default styles = StyleSheet.create({ 
   // App
   container: {
       flex: 1,
